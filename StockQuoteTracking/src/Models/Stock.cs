f64f3bd8c5f201e0e5bc40ce5183b7fb219e4643
Emanuela Quizini <manu.biq@gmail.com>
2024-12-16 22:07:30 -0300

    Add project files.

---
+++ StockQuoteTracking/src/Models/Stock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockQuoteTracking.src.Models
+{
+    public class Stock
+    {
+        public string Name { get; set; }
+        public double SalePrice { get; set; }
+        public double PurchasePrice { get; set; }
+
+        public Stock(string name, double salePrice, double purchasePrice)
+        {
+            Name = name;
+            SalePrice = salePrice;
+            PurchasePrice = purchasePrice;
+        }
+    }
+
+}
---
+++ StockQuoteTracking/src/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using StockQuoteTracking.src.Models;
+
+namespace StockQuoteTracking.src
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            if (args.Length < 3)
+            {
+                Console.WriteLine("You need to send the three arguments! \n " +
+                    "StcokQuoteTracking.exe <stock><sale-price><purchase-price>");
+                return;
+            }
+
+            string stockName = args[0];
+            double salePrice = double.Parse(args[1]);
+            double purchasePrice = double.Parse(args[2]);
+
+            Stock stock = new Stock(stockName, salePrice, purchasePrice);
+
+            Console.WriteLine(JsonSerializer.Serialize(stock));
+
+        }
+    }
+}
